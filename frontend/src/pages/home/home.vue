6c48885252d739630cd06a1cb7a7f0f4c11d51b0
Max Alexeev <ma.a.alekseev@labmedia.su>
2024-07-04 09:52:34 +0300

    dark theme test

--- frontend/src/pages/home/home.vue
+++ frontend/src/pages/home/home.vue
@@ -1,3 +1,62 @@
 <template>
-  <main class="mt-8"></main>
+  <main class="mt-8">
+    <button class="bg-secondary text-on-secondary ripple">
+      <div class="overlay"></div>
+      <div class="content">Tap</div>
+    </button>
+  </main>
 </template>
+
+<style lang="scss" scoped>
+button {
+  all: unset;
+  padding: 0.75rem 3rem;
+  border-radius: 0.5rem;
+  position: relative;
+  cursor: pointer;
+  overflow: hidden;
+
+  .overlay {
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    width: 100%;
+    height: 100%;
+    transition: all 0.2s ease;
+  }
+
+  :hover {
+    background: color-mix(in srgb, var(--g-theme-on-primary) 10%, transparent);
+  }
+
+  &.ripple {
+    &:after {
+      content: '';
+      position: absolute;
+      left: 50%;
+      top: 50%;
+      transform: translate(-50%, -50%) scale(1);
+      width: 100%;
+      aspect-ratio: 1;
+      border-radius: 50%;
+      background-color: color-mix(
+        in srgb,
+        var(--g-theme-on-primary) 20%,
+        transparent
+      );
+      opacity: 0;
+      transition:
+        opacity 0.3s ease,
+        transform 0.3s ease;
+    }
+
+    &:active:after {
+      transform: translate(-50%, -50%) scale(0);
+      opacity: 1;
+      transition: 0s;
+    }
+  }
+}
+</style>
--- frontend/src/shared/theme/config.js
+++ frontend/src/shared/theme/config.js
@@ -1,4 +1,23 @@
-export const THEME_CONFIG = {
+export const DARK = {
+  colors: {
+    primary: '#bd6fed',
+    secondary: '#f26161',
+    surface: '#262626',
+    background: '#1a1a1a',
+    error: '#EF476F',
+    warning: '#FF9F1C',
+    success: '#06D6A0',
+    'on-primary': '#1a1a1a',
+    'on-secondary': '#1a1a1a',
+    'on-surface': '#dbdbdb',
+    'on-background': '#dbdbdb',
+    'on-error': '#dbdbdb',
+    'on-warning': '#dbdbdb',
+    'on-success': '#dbdbdb'
+  }
+}
+
+export const BLUE = {
   colors: {
     primary: '#00A6FB',
     secondary: '#0582CA',
@@ -16,3 +35,5 @@ export const THEME_CONFIG = {
     'on-success': '#1A1A1A'
   }
 }
+
+export const THEME_CONFIG = DARK
