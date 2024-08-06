1672cafb308104575a848ab05721b56c6847f740
Maxim Alekseev <112779335+Alemy75@users.noreply.github.com>
2024-08-06 07:58:49 +0300

    add:init userData

--- frontend/src/pages/home/home.vue
+++ frontend/src/pages/home/home.vue
@@ -1,5 +1,19 @@
+<script setup>
+import { onMounted, ref } from 'vue'
+
+const userData = ref(null)
+
+onMounted(() => {
+  userData.value = window.Telegram.initDataUnsafe.user
+})
+</script>
+
 <template>
-  <main class="mt-8"></main>
+  <main class="mt-8">
+    <div v-if="userData">
+      Hello, {{ userData.firstName }} {{ userData.lastName }}!
+    </div>
+  </main>
 </template>
 
 <style lang="scss" scoped></style>
